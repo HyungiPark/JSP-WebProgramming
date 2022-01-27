e64e3fd3e3a30c7081249dab9c8caaddb1b2955b
HyungiPark <97497698+HyungiPark@users.noreply.github.com>
2022-01-27 17:23:14 +0900

    Update TestConnect.java

--- TestConnect.java
+++ TestConnect.java
@@ -1,69 +1,90 @@
-<%@ page language="java" contentType="text/html; charset=EUC-KR"
-    pageEncoding="EUC-KR"%>
-<%@ page import = "java.sql.*" %>
-<table align="center" border="1">
-	<tr>
-		<th>고객번호</th>
-		<th>이름</th>
-		<th>전화번호</th>
-		<th>주소</th>
-		<th>가입날짜</th>
-		<th>등급</th>
-		<th>시/도</th>
-	</tr>
-<% 
-	// 오라클 데이터베이스 연결
-	Class.forName("oracle.jdbc.OracleDriver");
-	Connection con = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/xe", "shop", "1234");
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ page import="java.sql.*"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" href="./css/common.css" />
+<title>JSP Oracle Test</title>
+</script>
+</head>
+<body>
+	<div class="container">
+		<div class="content">
+			<form action="TestConnect.jsp">
+				<input type="text" name="name"><input type="submit" value="조회">
+			</form>
+			<table align="center" border="1">
+				<tr>
+					<th>고객번호</th>
+					<th>이름</th>
+					<th>전화번호</th>
+					<th>주소</th>
+					<th>가입날짜</th>
+					<th>등급</th>
+					<th>시/도</th>
+				</tr>
+				<%
+				// 오라클 데이터베이스 연결
+				Class.forName("oracle.jdbc.OracleDriver");
+				Connection con = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/xe", "shop", "1234");
+				
+				String name = request.getParameter("name");
+				out.println("이름은 "+name+"입니다");
+				
+				int custno = 0;
+				String custname = "";
+				String phone = "";
+				String address = "";
+				Date joindate;
+				String grade = "";
+				String city = "";
+				String query = "select custno from member_tbl_02 where custname = '이축복'";
+				ResultSet result;
+				/* Connection con = getConnection(); */
+				//	result = con.prepareStatement(query).executeQuery();
+				//	result.next();					// 쿼리문 조회 결과 첫번째 행으로 이동. next를 안하면 시작시 오류 발생.
 
-	int custno = 0;
-	String custname = "";
-	String phone = "";
-	String address = "";
-	Date joindate;
-	String grade = "";
-	String city = "";
-	String query = "select custno from member_tbl_02 where custname = '이축복'";
-	ResultSet result;
-	/* Connection con = getConnection(); */
-//	result = con.prepareStatement(query).executeQuery();
-//	result.next();					// 쿼리문 조회 결과 첫번째 행으로 이동. next를 안하면 시작시 오류 발생.
-	
-	String query2 = "select * from member_tbl_02";
-	result = con.prepareStatement(query2).executeQuery();
+				String query2 = "select * from member_tbl_02";
+				result = con.prepareStatement(query2).executeQuery();
 
-	while(result.next()){
-		custno = result.getInt("CUSTNO");
-		custname = result.getString("CUSTNAME");
-		phone = result.getString("PHONE");
-		address = result.getString("ADDRESS");
-		joindate = result.getDate("JOINDATE");
-		grade = result.getString("GRADE");
-		city = result.getString("CITY");
-		
-		switch(grade){
-		case"A":
-			grade="VIP";
-			break;
-		case"B":
-			grade="GOLD";
-			break;
-		default:
-			grade="SIVER";
-			break;
-		}
-%>
-	<tr>
-		<td align="center"><%=custno %></td>
-		<td align="center"><%=custname %></td>
-		<td align="center"><%=phone %></td>
-		<td align="center"><%=address %></td>
-		<td align="center"><%=joindate %></td>
-		<td align="center"><%=grade %></td>
-		<td align="center"><%=city %></td>
-	</tr>
+				while (result.next()) {
+					custno = result.getInt("CUSTNO");
+					custname = result.getString("CUSTNAME");
+					phone = result.getString("PHONE");
+					address = result.getString("ADDRESS");
+					joindate = result.getDate("JOINDATE");
+					grade = result.getString("GRADE");
+					city = result.getString("CITY");
 
-<%			
-	}
-%>
-</table>
+					switch (grade) {
+					case "A":
+						grade = "VIP";
+						break;
+					case "B":
+						grade = "GOLD";
+						break;
+					default:
+						grade = "SIVER";
+						break;
+					}
+				%>
+				<tr>
+					<td align="center"><%=custno%></td>
+					<td align="center"><%=custname%></td>
+					<td align="center"><%=phone%></td>
+					<td align="center"><%=address%></td>
+					<td align="center"><%=joindate%></td>
+					<td align="center"><%=grade%></td>
+					<td align="center"><%=city%></td>
+				</tr>
+
+				<%
+				}
+				%>
+			</table>
+		</div>
+	</div>
+</body>
+</html>
